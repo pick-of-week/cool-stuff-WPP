34ed3a5bd16088e43c7577aba7483ac45514ad1a
Veer <obiwan@veersingh.ca>
2021-06-14 14:17:31 -0600

    Removed Personal Info

--- Backend/getProducts/settings/apiSettings.py
+++ Backend/getProducts/settings/apiSettings.py
@@ -1,14 +1,14 @@
 #Chinabrands API Settings START
-chinabrandsEmail = "ron.singh@gmail.com"
-chinabrandsPassword = "123Harneeta!@#"
-chinabrandsClientId = "2667118287"
+chinabrandsEmail = "Your Chinabrands Email Here"
+chinabrandsPassword = "Chinabrands Password Here"
+chinabrandsClientId = "Chinabrands Client Id"
 chinabrandsTokenAuthData = {"email" : chinabrandsEmail, "password" : chinabrandsPassword, "client_id" : chinabrandsClientId}
 chinabrandsLoginURL = "https://gloapi.chinabrands.com/v2/user/login"
-chinabrandsClientSecret = "f59fcdd9c3695cd56b789fac91455d86"
+chinabrandsClientSecret = "Chinabrands Client Secret"
 #Chinabrands API Settings END
 
 #Google Sheets API START
 googleSheetScope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-googleSheetURL = 'https://docs.google.com/spreadsheets/d/1LV7TFeRccDfbumk5cTay2VQvcgxch1LJdueagu9BHH4/edit#gid=816616519'
-googleSheetName = "Products"
-#Google Sheets API End
+googleSheetURL = 'Your Google Sheet URL Here'
+googleSheetName = "Sheet Name Here"
+#Google Sheets API 
